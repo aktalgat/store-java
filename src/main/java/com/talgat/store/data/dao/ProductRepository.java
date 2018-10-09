64311d31ee5f338e5297d6eb6e43f4bfe49fccbb
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-09 22:20:11 +0600

    ProductRepository to CRUD operations

---
+++ src/main/java/com/talgat/store/data/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.talgat.store.data.dao;
+
+import com.talgat.store.data.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+    @Override
+    List<Product> findAll();
+}
