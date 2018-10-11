85a8578485788eec19c88d831354967963561af3
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-11 17:29:30 +0600

    Add to Category name unique constraint

--- src/main/java/com/talgat/store/data/model/Category.java
+++ src/main/java/com/talgat/store/data/model/Category.java
@@ -12,6 +12,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     public Category() {}
