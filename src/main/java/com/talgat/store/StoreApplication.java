bbde4940c5ae17818a6e8a7cd2adfcca51bd4191
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-15 16:42:01 +0600

    Enable secured annotation and protect protected controller

--- src/main/java/com/talgat/store/StoreApplication.java
+++ src/main/java/com/talgat/store/StoreApplication.java
@@ -2,8 +2,12 @@ package com.talgat.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true)
 public class StoreApplication {
 
     public static void main(String[] args) {
