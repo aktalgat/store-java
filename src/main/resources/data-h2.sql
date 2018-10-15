a22da5ac3b11273011702c8b1bf200b230bca56e
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-15 16:24:23 +0600

    Change user password hash

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -5,7 +5,7 @@ MERGE INTO roles KEY(id) VALUES
 --Password encoder https://www.dailycred.com/article/bcrypt-calculator
 --Max number round 10
 MERGE INTO users KEY(id) values
-(2, 'user01@email.em', 'User01', '$2a$10$BGanhgbFSeh7GYd76EkcluEtQSTgURpEDIq6jJXl6jrWWeWIFwbIq', 'user01');
+(2, 'user01@email.em', 'User01', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'user01');
 
 MERGE INTO user_roles KEY(user_id, role_id) values
 (2, 2);
