a2600e7aa507f9bcf525ae913bcd42b9404c2739
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-15 16:42:23 +0600

    Add user with role user

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -5,10 +5,9 @@ MERGE INTO roles KEY(id) VALUES
 --Password encoder https://www.dailycred.com/article/bcrypt-calculator
 --Max number round 10
 MERGE INTO users KEY(id) values
+(1, 'aktalgat@mail.ru', 'Talgat', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'talgat'),
 (2, 'user01@email.em', 'User01', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'user01');
 
 MERGE INTO user_roles KEY(user_id, role_id) values
+(1, 1),
 (2, 2);
-
-delete from user_roles where user_id=1;
-delete from users where id=1;
