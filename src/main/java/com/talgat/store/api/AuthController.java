06e951564da67fc673681bfe13a550fbb905e09e
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-15 16:24:41 +0600

    Auth controller to sign in

---
+++ src/main/java/com/talgat/store/api/AuthController.java
@@ -0,0 +1,45 @@
+package com.talgat.store.api;
+
+import com.talgat.store.api.payload.AuthRequest;
+import com.talgat.store.api.payload.JwtAuthenticationResponse;
+import com.talgat.store.security.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+public class AuthController extends AbstractPublicApi {
+    private AuthenticationManager authenticationManager;
+    private JwtTokenProvider tokenProvider;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @PostMapping("/auth/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody AuthRequest authRequest) {
+        log.info("Get request for login: {}", authRequest);
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getLogin(),
+                        authRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+}
