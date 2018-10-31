039b1d1b6acbfee6ed4f19c93049ba31783526c0
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-31 11:24:23 +0600

    Fix warning hash equals call super false

--- src/main/java/com/talgat/store/api/payload/ItemResponse.java
+++ src/main/java/com/talgat/store/api/payload/ItemResponse.java
@@ -1,8 +1,10 @@
 package com.talgat.store.api.payload;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class ItemResponse extends AbstractApiResponse {
     private long id;
 
