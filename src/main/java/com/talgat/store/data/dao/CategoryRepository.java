897fed52fe54d0bc23652d4b447b0818c5e2f57e
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-13 17:54:54 +0600

    Category repository find by id

--- src/main/java/com/talgat/store/data/dao/CategoryRepository.java
+++ src/main/java/com/talgat/store/data/dao/CategoryRepository.java
@@ -4,8 +4,12 @@ import com.talgat.store.data.model.Category;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
     @Override
     List<Category> findAll();
+
+    @Override
+    Optional<Category> findById(Long id);
 }
