8019051260e481c5ee7c1fe419ee9a5274231262
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-19 10:32:14 +0600

    Renamed default user and admin

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -4,9 +4,10 @@ MERGE INTO roles KEY(id) VALUES
 
 --Password encoder https://www.dailycred.com/article/bcrypt-calculator
 --Max number round 10
+--Default password 12345
 MERGE INTO users KEY(id) values
-(1, 'aktalgat@mail.ru', 'Talgat', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'talgat'),
-(2, 'user01@email.em', 'User01', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'user01');
+(1, 'user@domain.com', 'User', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'user'),
+(2, 'admin@domain.com', 'Admin', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', 'admin');
 
 MERGE INTO user_roles KEY(user_id, role_id) values
 (1, 1),
