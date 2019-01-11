4c44a059cc1250fbbdcad2ca794308cdd4f705da
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-11 10:08:21 +0600

    Integration test 1

--- src/test/java/com/talgat/store/api/ProductPublicControllerTestIT.java
+++ src/test/java/com/talgat/store/api/ProductPublicControllerTestIT.java
@@ -2,13 +2,13 @@ package com.talgat.store.api;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 class ProductPublicControllerTestIT {
 
     @Test
