f72075d5b2007adbd01b6adb8e0e48bd652fd8ed
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-14 21:39:37 +0600

    Test

---
+++ src/test/java/com/talgat/store/api/ProductProtectedControllerTest.java
@@ -0,0 +1,21 @@
+package com.talgat.store.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductProtectedControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void getProductPublicList() {
+    }
+}
