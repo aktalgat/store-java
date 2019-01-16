586ec9d68baa55ae7de25f20a121ce62abcb92bc
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-16 11:28:52 +0600

    Test 3

---
+++ src/test/java/com/talgat/store/api/AuthControllerTest.java
@@ -0,0 +1,17 @@
+package com.talgat.store.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void authenticateUser() {
+    }
+}
