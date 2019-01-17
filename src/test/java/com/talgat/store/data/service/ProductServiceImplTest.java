691a57625169e71e84d816ff8bc93237883d390a
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-17 17:11:49 +0600

    Test service

---
+++ src/test/java/com/talgat/store/data/service/ProductServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.talgat.store.data.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void saveProduct1() {
+    }
+
+    @Test
+    void getProductList() {
+    }
+}
