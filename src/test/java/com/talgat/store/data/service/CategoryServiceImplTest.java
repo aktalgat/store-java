1be862e69d9390a154d65db58f4d3083522ec1f1
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-18 11:22:02 +0600

    Test service 2

---
+++ src/test/java/com/talgat/store/data/service/CategoryServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.talgat.store.data.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveCategory() {
+    }
+
+    @Test
+    void saveCategory1() {
+    }
+
+    @Test
+    void getCategoryList() {
+    }
+}
