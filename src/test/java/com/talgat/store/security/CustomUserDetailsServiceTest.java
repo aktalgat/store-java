3f4fca4c04a5819227cb116d4dbc35626fa2e1cc
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-20 12:58:26 +0600

    Test jwt

---
+++ src/test/java/com/talgat/store/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,21 @@
+package com.talgat.store.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomUserDetailsServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void loadUserById() {
+    }
+}
