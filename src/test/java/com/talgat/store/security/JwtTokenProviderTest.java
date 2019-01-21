963058a735a36cfb2c1c154a2f6e04944b398096
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-21 16:09:02 +0600

    Entry point test

---
+++ src/test/java/com/talgat/store/security/JwtTokenProviderTest.java
@@ -0,0 +1,25 @@
+package com.talgat.store.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class JwtTokenProviderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void generateToken1() {
+    }
+
+    @Test
+    void getUserIdFromJWT1() {
+    }
+
+    @Test
+    void validateToken1() {
+    }
+}
