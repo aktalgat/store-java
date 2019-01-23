3382758a1176f28098894f88aa623f178b75cf95
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-23 10:40:51 +0600

    Test jwt entry point

---
+++ src/test/java/com/talgat/store/security/JwtAuthenticationEntryPointTest.java
@@ -0,0 +1,17 @@
+package com.talgat.store.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtAuthenticationEntryPointTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void commence() {
+    }
+}
