d05b915e518adf5dc93670972755b9ba8c5f9112
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-30 12:00:30 +0600

    Delete incorrect test

--- src/test/java/com/talgat/store/api/ProductPublicControllerTestIT.java
+++ src/test/java/com/talgat/store/api/ProductPublicControllerTestIT.java
@@ -1,14 +1,9 @@
 package com.talgat.store.api;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 class ProductPublicControllerTestIT {
 
     @Test
