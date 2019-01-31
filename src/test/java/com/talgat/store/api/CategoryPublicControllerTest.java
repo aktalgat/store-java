27a474630e0315015fecdf351b68400dc94a47d3
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-31 11:34:13 +0600

    Category controller test

--- src/test/java/com/talgat/store/api/CategoryPublicControllerTest.java
+++ src/test/java/com/talgat/store/api/CategoryPublicControllerTest.java
@@ -1,17 +1,28 @@
 package com.talgat.store.api;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.talgat.store.data.model.Category;
+import com.talgat.store.data.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryPublicControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryPublicController categoryPublicController;
 
     @Test
     void getCategoryPublicList() {
+        List<Category> list = categoryPublicController.getCategoryPublicList();
     }
 }
