a2ea06bc1d519c775e1127712edabdad9f69c396
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-11 13:28:03 +0600

    Category service set transactional

--- src/main/java/com/talgat/store/data/service/CategoryServiceImpl.java
+++ src/main/java/com/talgat/store/data/service/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -20,6 +21,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional
     public Category saveCategory(CategoryRequest categoryRequest) {
         Category category = new Category(categoryRequest.getName());
 
@@ -27,6 +29,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional
     public Category saveCategory(Category category) {
         Category newCategory;
         try {
@@ -44,6 +47,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional
     public long update(CategoryRequest categoryRequest) {
         Category category = categoryRepository.findById(categoryRequest.getId())
                 .orElseThrow(() -> new InternalException("Couldn't find Category with such id"));
