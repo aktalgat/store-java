9ac7c278483c9aa954af61a00e6eb1f0803d7833
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-11 13:35:31 +0600

    Add findById method to repository

--- src/main/java/com/talgat/store/data/dao/ProductRepository.java
+++ src/main/java/com/talgat/store/data/dao/ProductRepository.java
@@ -4,9 +4,13 @@ import com.talgat.store.data.model.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     @Override
     List<Product> findAll();
+
+    @Override
+    Optional<Product> findById(Long id);
 }
