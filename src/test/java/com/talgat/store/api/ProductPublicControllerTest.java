a091feb12c432f5ecde00a1400f77c13144556d6
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-17 19:47:52 +0600

    Correct mock test

--- src/test/java/com/talgat/store/api/ProductPublicControllerTest.java
+++ src/test/java/com/talgat/store/api/ProductPublicControllerTest.java
@@ -3,12 +3,16 @@ package com.talgat.store.api;
 import com.talgat.store.data.dao.ProductRepository;
 import com.talgat.store.data.model.Product;
 import com.talgat.store.data.service.ProductService;
+import com.talgat.store.data.service.ProductServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,14 +23,24 @@ class ProductPublicControllerTest {
     @Mock
     ProductRepository productRepository;
 
-    @Mock
-    ProductService productService;
-
     @InjectMocks
+    ProductServiceImpl productService;
+
     ProductPublicController publicController;
+
+    @BeforeEach
+    void init() {
+        publicController = new ProductPublicController(productService);
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product(1L, "Молоко", "Молоко", "Молоко", "Молоко", "-10", 30D, 35D, 5));
+        Mockito.lenient().when(productRepository.findAll()).thenReturn(list);
+    }
 
     @Test
     void getProductList() {
         List<Product> list = publicController.getProductList();
+        System.out.println(list.size());
+        list.forEach(System.out::println);
     }
 }
